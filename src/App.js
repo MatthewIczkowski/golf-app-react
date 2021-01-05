66ef9efa657fa86e98ea227a839451fdfe8ac624
MatthewIczkowski <iczkom13@gmail.com>
2021-01-05 08:53:24 -0500

    sidebar style updates and toggle onClick implementation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import './App.css';
-import Navbar from "./components/Navbar";
 import {BrowserRouter as Router} from 'react-router-dom'
-import Siderbar from './components/Sidebar';
+import Home from './pages';
 
 function App() {
   return (
     <Router>
-      <Siderbar />
-      <Navbar />
+      <Home />
     </Router>
   );
 }
--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,12 +1,23 @@
 import React from 'react'
-import { SidebarContainer, Icon, CloseIcon } from './SidebarElements';
+import { SidebarContainer, Icon, CloseIcon, SidebarWrapper, SidebarMenu, SidebarLink, SideBtnWrap, SidebarRoute } from './SidebarElements';
 
-const Sidebar = () => {
+const Sidebar = ({isOpen, toggle}) => {
     return (
-        <SidebarContainer>
-             <Icon>
+        <SidebarContainer isOpen={isOpen} onClick={toggle}>
+             <Icon onClick={toggle}>
                  <CloseIcon />
              </Icon>
+             <SidebarWrapper>
+                 <SidebarMenu>
+                    <SidebarLink to='about'>About</SidebarLink>
+                    <SidebarLink to='discover'>Discover</SidebarLink>
+                    <SidebarLink to='features'>Features</SidebarLink>
+                    <SidebarLink to='signup'>Sign Up</SidebarLink>
+                 </SidebarMenu>
+                 <SideBtnWrap>
+                     <SidebarRoute to="/signin"> Sign In</SidebarRoute>
+                 </SideBtnWrap>
+             </SidebarWrapper>
         </SidebarContainer>
     )
 }
