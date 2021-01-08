b2ddc7f1f2483cf1cc7acc7c2275cb68aa87b2f9
MatthewIczkowski <iczkom13@gmail.com>
2021-01-08 09:34:45 -0500

    added footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import {FooterContainer, FooterWrap, FooterLinksContainer, FooterLinksWrapper, FooterLinkItems, FooterLinkTitle, FooterLink} from './FooterElements'
+
+const Footer = () => {
+    return (
+        <FooterContainer>
+            <FooterWrap>
+                <FooterLinksContainer>
+                    <FooterLinksWrapper>
+                        <FooterLinkItems>
+                            <FooterLinkTitle>About Us</FooterLinkTitle>
+                                <FooterLink to="/signin">How it works</FooterLink>
+                                <FooterLink to="/signin">Testimonials</FooterLink>
+                                <FooterLink to="/signin">Careers</FooterLink>
+                                <FooterLink to="/signin">Investors</FooterLink>
+                                <FooterLink to="/signin">Terms of Service</FooterLink>
+                        </FooterLinkItems>
+                        <FooterLinkItems>
+                            <FooterLinkTitle>About Us</FooterLinkTitle>
+                                <FooterLink to="/signin">How it works</FooterLink>
+                                <FooterLink to="/signin">Testimonials</FooterLink>
+                                <FooterLink to="/signin">Careers</FooterLink>
+                                <FooterLink to="/signin">Investors</FooterLink>
+                                <FooterLink to="/signin">Terms of Service</FooterLink>
+                        </FooterLinkItems>
+                    </FooterLinksWrapper>
+                    <FooterLinksWrapper>
+                        <FooterLinkItems>
+                            <FooterLinkTitle>About Us</FooterLinkTitle>
+                                <FooterLink to="/signin">How it works</FooterLink>
+                                <FooterLink to="/signin">Testimonials</FooterLink>
+                                <FooterLink to="/signin">Careers</FooterLink>
+                                <FooterLink to="/signin">Investors</FooterLink>
+                                <FooterLink to="/signin">Terms of Service</FooterLink>
+                        </FooterLinkItems>
+                        <FooterLinkItems>
+                            <FooterLinkTitle>About Us</FooterLinkTitle>
+                                <FooterLink to="/signin">How it works</FooterLink>
+                                <FooterLink to="/signin">Testimonials</FooterLink>
+                                <FooterLink to="/signin">Careers</FooterLink>
+                                <FooterLink to="/signin">Investors</FooterLink>
+                                <FooterLink to="/signin">Terms of Service</FooterLink>
+                        </FooterLinkItems>
+                    </FooterLinksWrapper>
+                </FooterLinksContainer>
+                <SocialMedia>
+                    <SocialMediaWrap>
+                        <SocialLogo to='/'>
+                            Golf-App
+                        </SocialLogo>
+                        <WebsiteRight>Golf-App © 2020</WebsiteRight>
+                    </SocialMediaWrap>
+                </SocialMedia>
+            </FooterWrap>
+        </FooterContainer>
+    )
+}
+
+export default Footer
